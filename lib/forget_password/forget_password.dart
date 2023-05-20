3bcdf9bb1d3e4b2e40d4713c8f81145a7dc28ef5
martink2004 <martykirilov48@gmail.com>
2023-05-20 15:51:58 +0300

    Update

--- lib/forget_password/forget_password.dart
+++ lib/forget_password/forget_password.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:project_flutter/forget_password/components/heading_text.dart';
 
 class ForgetPasswordScreen extends StatelessWidget {
 
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.pink,Colors.deepOrange.shade300],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          stops: const [0.2,0.9],
+        ),
+      ),
+      child:  Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                HeadText(),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
